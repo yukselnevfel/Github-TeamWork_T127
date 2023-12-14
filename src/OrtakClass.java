44e2ab9435c41d91be330a79c8bb424a7240bd59
yukselnevfel <nyuksel3410@gmail.com>
2023-12-14 11:41:34 +0300

    github colobrate

---
+++ src/OrtakClass.java
@@ -0,0 +1,7 @@
+public class OrtakClass {
+    public static void main(String[] args) {
+        System.out.println("ortak class olusturuldu!");
+
+
+    }
+}
---
+++ src/nevfel.java
@@ -0,0 +1,15 @@
+public class nevfel {
+    public static void main(String[] args) {
+        System.out.println("nevfel'in mekani:)");
+
+
+        /*
+        git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yukselnevfel/Github-TeamWork_T127.git
+git push -u origin main
+         */
+    }
+}
